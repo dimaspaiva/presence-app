5c8a83fc9769c2f23b5428c39e657109d667a55e
dimas <dimasalpaiva@gmail.com>
2025-04-29 01:53:47 -0300

    feat: add create example and parse csv input file to csv service

--- src/services/csv.service.ts
+++ src/services/csv.service.ts
@@ -2,12 +2,52 @@ import { Attendance } from "../types/Attendance";
 
 export function mapAttendanceDataToCsvString(attendanceData: Attendance, presentFormat = '.', absentFormat = ' '): string {
   const header = "Número,Nome,Presença\n"
-
   const body = attendanceData.reduce((result, user) => result.concat(
     `${user.number},${user.name},${user.isPresent ? presentFormat : absentFormat}\n`),
     '');
 
   return `${header}${body}`;
+}
+
+export function buildUsersExampleCsvData(): string {
+  const header = "Número,Nome"
+  const body = "1,Nome 1\n2,Nome 2\n3,Nome 3";
+
+  return `${header}\n${body}`;
+}
+
+export function parseCsvFile(file: File) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      if (typeof event.target?.result !== 'string') {
+        reject(new Error('File reading error'));
+        return;
+      }
+
+      const text = event.target.result;
+      const [header, ...lines] = text.split('\n');
+      const objectKeys = header.split(',');
+
+      const noEmptyLineData = lines.filter((line) => Boolean(line))
+      const data = noEmptyLineData.map(line => {
+        const values = line.split(',');
+
+        return objectKeys.reduce((obj: Record<string, string>, key, index) => {
+          obj[key.trim()] = values[index].trim();
+          return obj;
+        }, {});
+      });
+
+      resolve(data);
+    };
+
+    reader.onerror = (error) => {
+      reject(error);
+    };
+
+    reader.readAsText(file);
+  });
 }
 
 export function downloadCsvFile(data: string, fileName: string) {
