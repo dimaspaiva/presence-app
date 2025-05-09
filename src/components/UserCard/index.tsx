02e1c287a08b85933d06547a0b8cc256ded52ec8
dimas <dimasalpaiva@gmail.com>
2025-05-08 22:06:07 -0300

    refactor: add cached functions to improve performance

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, useState } from "react";
 import { User } from "../../types/User";
 import { AttendanceEnum } from "../../types/Attendance";
 
 import "./styles.css";
+import { useUserCard } from "./useUserCard";
 
 export type UserProps = {
   user: User;
@@ -10,130 +10,25 @@ export type UserProps = {
 };
 
 export function UserCard({ user, applyUserAttendance }: UserProps) {
-  const [selectedAnimation, setSelectedAnimation] = useState<
-    AttendanceEnum | ""
-  >("");
-  const cardRef = useRef<HTMLDivElement>(null);
-  const [avatarUrl, setAvatarUrl] = useState<string>("#");
-  const [isLoading, setIsLoading] = useState(false);
-  const [fade, setFade] = useState(false);
-  const [cardPosition, setCardPosition] = useState<{ x: number; y: number }>({
-    x: 0,
-    y: 0,
-  });
-  const [rotate, setRotate] = useState("0deg");
-
-  function startCardMovement(event: TouchEvent) {
-    const x = event.touches[0].clientX;
-    const y = event.touches[0].clientY;
-
-    event.target?.addEventListener(
-      "touchmove",
-      buildMoveCard({ x, y }) as EventListenerOrEventListenerObject
-    ); // TODO Improve type
-    event.target?.addEventListener(
-      "touchend",
-      buildResetCard({ x, y }) as EventListenerOrEventListenerObject
-    ); // TODO Improve type
-  }
-
-  function buildMoveCard(initialPosition: { x: number; y: number }) {
-    return (event: TouchEvent) => {
-      event.preventDefault();
-      const x = event.touches[0].clientX;
-      const y = event.touches[0].clientY;
-
-      const newX = x - initialPosition.x;
-      const newY = y - initialPosition.y;
-
-      if (newX > 0) {
-        setRotate(`${Math.min(newX * 0.1, 45)}deg`);
-      } else {
-        setRotate(`${Math.max(newX * 0.1, -45)}deg`);
-      }
-      setCardPosition({ x: newX, y: newY });
-    };
-  }
-
-  const MINIMAL_DISTANCE = 150;
-  function buildResetCard(initialPosition: { x: number; y: number }) {
-    return (event: TouchEvent) => {
-      if (
-        initialPosition.x - event.changedTouches[0].clientX >
-        MINIMAL_DISTANCE
-      ) {
-        return applyUserAttendance(user.id, AttendanceEnum.PRESENT);
-      }
-
-      if (
-        initialPosition.x - event.changedTouches[0].clientX <
-        -MINIMAL_DISTANCE
-      ) {
-        return applyUserAttendance(user.id, AttendanceEnum.ABSENT);
-      }
-
-      setCardPosition({ x: 0, y: 0 });
-      setRotate("0deg");
-    };
-  }
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetch(`https://api.dicebear.com/9.x/bottts/svg?seed=${user.id}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.blob();
-      })
-      .then((blob) => {
-        const url = URL.createObjectURL(blob);
-        setAvatarUrl(url || "#");
-      })
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [user.id]);
-
-  useEffect(() => {
-    cardRef.current?.addEventListener("touchstart", startCardMovement);
-    const card = cardRef.current;
-
-    return () => {
-      card?.removeEventListener("touchstart", startCardMovement);
-    };
-  }, [cardRef]);
-
-  const createAnimationFunction = (attendanceType: AttendanceEnum) => {
-    return () => {
-      setSelectedAnimation(attendanceType);
-      setTimeout(() => {
-        setFade(true);
-      }, 100);
-
-      setTimeout(() => {
-        applyUserAttendance(user.id, attendanceType);
-      }, 300);
-    };
-  };
-
-  const fadeClass = fade ? "fade" : "";
+  const {
+    avatarUrl,
+    cardPosition,
+    cardRef,
+    userCardClassList,
+    isLoading,
+    createAnimationFunction,
+  } = useUserCard(applyUserAttendance, user);
 
   return (
     <div
       data-testid="user-card"
-      className={`user-card_container card ${
-        user.number
-      } ${selectedAnimation.toLowerCase()} ${fadeClass}`}
+      className={userCardClassList}
       draggable
       ref={cardRef}
       style={{
         marginLeft: cardPosition.x,
         marginTop: cardPosition.y,
-        rotate,
+        rotate: cardPosition.rotate,
       }}
     >
       <div className="user-data_container">
@@ -144,15 +39,9 @@ export function UserCard({ user, applyUserAttendance }: UserProps) {
         <div className="avatar-circle">
           {isLoading && <div className="avatar_loading-spinner"></div>}
           {!isLoading && (
-            <img
-              className={`user-avatar ${fadeClass}`}
-              src={avatarUrl}
-              alt={user.name}
-            />
+            <img className="user-avatar" src={avatarUrl} alt={user.name} />
           )}
-          {!isLoading && !avatarUrl && (
-            <div className={`user-avatar ${fadeClass}`}></div>
-          )}
+          {!isLoading && !avatarUrl && <div className="user-avatar"></div>}
         </div>
       </div>
       <div className="user-card_actions">
---
+++ src/components/UserCard/useUserCard.ts
@@ -0,0 +1,160 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { AttendanceEnum } from "../../types/Attendance";
+import { User } from "../../types/User";
+
+export type ApplyUserAttendanceFunction = (userId: string, attendance: AttendanceEnum) => void
+
+export function useUserCard(applyUserAttendance: ApplyUserAttendanceFunction, user: User) {
+  const [selectedAnimation, setSelectedAnimation] = useState<
+    AttendanceEnum | ""
+  >("");
+  const cardRef = useRef<HTMLDivElement>(null);
+  const [avatarUrl, setAvatarUrl] = useState<string>("#");
+  const [isLoading, setIsLoading] = useState(false);
+  const [fade, setFade] = useState(false);
+  const [cardPosition, setCardPosition] = useState<{
+    x: number;
+    y: number;
+    rotate: string;
+  }>({
+    x: 0,
+    y: 0,
+    rotate: "0deg",
+  });
+
+  const buildResetCard = useCallback(
+    (initialPosition: { x: number; y: number }, applyUserAttendance: ApplyUserAttendanceFunction, userId: string) => {
+      return (event: TouchEvent) => {
+        if (
+          initialPosition.x - event.changedTouches[0].clientX >
+          MINIMAL_DISTANCE
+        ) {
+          return applyUserAttendance(userId, AttendanceEnum.PRESENT);
+        }
+
+        if (
+          initialPosition.x - event.changedTouches[0].clientX <
+          -MINIMAL_DISTANCE
+        ) {
+          return applyUserAttendance(userId, AttendanceEnum.ABSENT);
+        }
+
+        setCardPosition({ x: 0, y: 0, rotate: "0deg" });
+      };
+    },
+    []
+  );
+
+  const calculateRotation = useCallback((distanceX: number) => {
+    if (distanceX > 0) {
+      return `${Math.min(distanceX * 0.1, 45)}deg`;
+    }
+
+    return `${Math.max(distanceX * 0.1, -45)}deg`;
+  }, []);
+
+  const buildMoveCard = useCallback(
+    (initialPosition: { x: number; y: number }) => {
+      return (event: TouchEvent) => {
+        event.preventDefault();
+        const x = event.touches[0].clientX;
+        const y = event.touches[0].clientY;
+
+        const newX = x - initialPosition.x;
+        const newY = y - initialPosition.y;
+
+        const rotate = calculateRotation(newX);
+        setCardPosition({ x: newX, y: newY, rotate });
+      };
+    },
+    [calculateRotation]
+  );
+
+  const startCardMovement = useCallback((event: TouchEvent) => {
+    const x = event.touches[0].clientX;
+    const y = event.touches[0].clientY;
+
+    if (!event.target) {
+      console.error("Missing event target");
+      return;
+    }
+
+    event.target.addEventListener(
+      "touchmove",
+      buildMoveCard({ x, y }) as EventListenerOrEventListenerObject,
+      {}
+    ); // TODO Improve type
+    event.target.addEventListener(
+      "touchend",
+      buildResetCard({ x, y }, applyUserAttendance, user.id) as EventListenerOrEventListenerObject
+    ); // TODO Improve type
+  },
+    [buildMoveCard, buildResetCard, applyUserAttendance, user.id]
+  );
+
+  const MINIMAL_DISTANCE = 150;
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://api.dicebear.com/9.x/bottts/svg?seed=${user.id}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.blob();
+      })
+      .then((blob) => {
+        const url = URL.createObjectURL(blob);
+        setAvatarUrl(url || "#");
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [user.id]);
+
+  useEffect(() => {
+    cardRef.current?.addEventListener("touchstart", startCardMovement);
+    const card = cardRef.current;
+
+    return () => {
+      card?.removeEventListener("touchstart", startCardMovement);
+    };
+  }, [cardRef, startCardMovement]);
+
+  const createAnimationFunction = useCallback(
+    (attendanceType: AttendanceEnum) => {
+      return () => {
+        setSelectedAnimation(attendanceType);
+        setTimeout(() => {
+          setFade(true);
+        }, 100);
+
+        setTimeout(() => {
+          applyUserAttendance(user.id, attendanceType);
+        }, 300);
+      };
+    },
+    [applyUserAttendance, user.id]
+  );
+
+
+  const userCardClassList = useMemo(() => {
+    const fadeClass = fade ? "fade" : ""
+
+    return `user-card_container card ${fadeClass} ${selectedAnimation.toLocaleLowerCase()}`
+  }, [fade, selectedAnimation])
+
+  return {
+    selectedAnimation,
+    cardRef,
+    avatarUrl,
+    isLoading,
+    fade,
+    cardPosition,
+    createAnimationFunction,
+    userCardClassList
+  }
+}
